113696acc201e9238e161a8a7a6805d9cb336fa2
Links789 <85124456+Links789@users.noreply.github.com>
2023-10-13 19:09:21 +0800

    Update: pyMain.py

--- py/pyMain.py
+++ py/pyMain.py
@@ -1,12 +1,14 @@
 import requests
 import json
-# import aardio
 
 def getImgSrc(iNumber=1):
-    res = requests.get(f"https://v.api.aa1.cn/api/api-tplist/go.php/api/picture/index?page={iNumber}",verify=False)
+    res = requests.get(f"https://v.api.aa1.cn/api/api-tplist/go.php/api/picture/index?page={iNumber}", verify=False)
     print(res.text)
-    data=json.loads(res.text)['data']
+    data = json.loads(res.text)['data']
     print(data)
-    ImgSrclist=['||'.join(pics['pics']) for pics in data]
-    return '||'.join(ImgSrclist) 
+    ImgSrclist = ['||'.join(pics['pics']) for pics in data]
+    return '||'.join(ImgSrclist)
 
+if __name__ == "__main__":
+    result = getImgSrc()
+    # print(result)
